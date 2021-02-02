8c6e5d692f8b225d8c0c7ac2dd2067795fd172fa
Akhil <akhiljagga01802@gmail.com>
2021-02-02 13:08:38 +0530

    signin , signup api

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require("express");
+const { userModal } = require("../modals/userModal");
+const { getToken } = require("../util");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+router.post("/register", async (req, res) => {
+  console.log(req.body);
+  const user = new userModal({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ message: "Invalid User Data." });
+  }
+});
+router.post("/signin", async (req, res) => {
+  const isUser = await userModal.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (isUser) {
+    res.send({
+      _id: isUser.id,
+      name: isUser.name,
+      email: isUser.email,
+      isAdmin: isUser.isAdmin,
+      token: getToken(isUser),
+    });
+  } else {
+    res.status(401).send({ message: "Invalid Email or Password." });
+  }
+});
+
+module.exports = router;
---
+++ util.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+exports.getToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET
+  );
+};
